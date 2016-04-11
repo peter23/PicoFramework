b32ab567969aead701b913f45b1cab624c933ecd
peter23 <i@peter23.com>
2016-04-12 01:25:46 +0300

    MyDate module with support of timezone

---
+++ app/modules/MyDate.php
@@ -0,0 +1,25 @@
+<?php
+
+	class Module_MyDate extends BaseModule {
+
+		public $dt;
+
+		public function __construct($db, $tz) {
+			$tz = new DateTimeZone($tz);
+			$this->dt = new DateTime();
+			$this->dt->setTimezone($tz);
+		}
+
+		public function fromInt($f, $t = null) {
+			if(is_null($t)) $t = time();
+			$this->dt->setTimestamp($t);
+			return $this->dt->format($f);
+		}
+
+		public function fromStr($f, $s) {
+			$t = strtotime($s);
+			if(!$t) return false;
+			return $this->fromInt($f, $t);
+		}
+
+	}
