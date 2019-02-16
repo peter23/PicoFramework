56b328bbd7df72587c59c44cbfcd23fd3df17a7f
peter23 <i@peter23.com>
2019-02-16 03:04:10 +0200

    updates

--- app/modules/Utils.php
+++ app/modules/Utils.php
@@ -23,8 +23,7 @@
 			return ob_get_clean();
 		}
 
-		public function generateRandomString($length) {
-			$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		public function generateRandomString($length, $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') {
 			$charactersLength = strlen($characters);
 			$randomString = '';
 			for($i = 0; $i < $length; $i++) {
@@ -34,26 +33,54 @@
 		}
 
 		public function cryptStr($s, $key = false) {
-			$iv = $this->generateRandomString(16);
+			$iv = $this->generateRandomString(8);
 			return $iv.openssl_encrypt(
-				gzdeflate($s),
-				'AES256',
+				gzdeflate(time().$s),
+				'BF',
 				$key ? $key : getConfig('crypt', 'key'),
 				0,
 				$iv
 			);
 		}
 
-		public function decryptStr($s, $key = false) {
-			$iv = substr($s, 0, 16);
-			$s = substr($s, 16);
-			return gzinflate(openssl_decrypt(
+		public function decryptStr($s, $key = false, $lifetime = false) {
+			$iv = substr($s, 0, 8);
+			$s = substr($s, 8);
+			$ret = gzinflate(openssl_decrypt(
 				$s,
-				'AES256',
+				'BF',
 				$key ? $key : getConfig('crypt', 'key'),
 				0,
 				$iv
 			));
+			$time = time();
+			$time_strlen = strlen($time);
+			$ret_time = substr($ret, 0, $time_strlen);
+			if( $lifetime && (($time - $ret_time) > $lifetime) ) {
+				return false;
+			} else {
+				return substr($ret, $time_strlen);
+			}
+		}
+
+		public function cryptStrSmall($s, $key = false, $iv = false) {
+			return openssl_encrypt(
+				$s,
+				'BF',
+				$key ? $key : getConfig('crypt', 'key_small'),
+				0,
+				$iv ? $iv : getConfig('crypt', 'iv_small')
+			);
+		}
+
+		public function decryptStrSmall($s, $key = false, $iv = false) {
+			return openssl_decrypt(
+				$s,
+				'BF',
+				$key ? $key : getConfig('crypt', 'key_small'),
+				0,
+				$iv ? $iv : getConfig('crypt', 'iv_small')
+			);
 		}
 
 	}
